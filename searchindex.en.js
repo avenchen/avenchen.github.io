d9ccc4709eca68ab5b2185cc41fe3a0af43afd2d
Aven Chen <avenchentw@gmail.com>
2025-05-27 21:36:04 +0800

    first commit

--- searchindex.en.js
+++ searchindex.en.js
@@ -1,14 +1,6 @@
 var relearn_searchindex = [
   {
-    "breadcrumb": "首頁",
-    "content": "擁有多元化的專業知識和超過20年的資訊科技工作經驗。致力於領導創新團隊，開發AI客服和CRM解決方案。目前正在攻讀大同大學的資工博士學位，為資安和AI領域結合理論和實務。",
-    "description": "擁有多元化的專業知識和超過20年的資訊科技工作經驗。致力於領導創新團隊，開發AI客服和CRM解決方案。目前正在攻讀大同大學的資工博士學位，為資安和AI領域結合理論和實務。",
-    "tags": [],
-    "title": "關於我",
-    "uri": "/me/index.html"
-  },
-  {
-    "breadcrumb": "首頁 \u003e \r軟體工程",
+    "breadcrumb": "軟體工程",
     "content": "人工智慧（Artificial Intelligence, AI）可以定義為模仿人類與人類思維相關的認知功能的機器或計算機，如學習和解決問題。它是計算機科學的一個分支，能夠感知其環境並採取行動，最大限度地提高其成功機會。此外，人工智慧能夠從過去的經驗中學習，做出合理的決策，並快速回應。因此，人工智慧研究人員的科學目標是通過構建具有象徵意義的推理或推理的計算機程式來理解智慧。\n以下是一些與人工智慧相關的關鍵詞：\n機器學習（Machine Learning, ML）：機器學習是 AI 交響樂中的一個工具，它是從先前的數據學習進行運算以預測行為的能力。機器學習最主流的方法是向算法顯示情境的數據集，並透過訓練模型告訴它什麼是正確的決定。 深度學習（Deep Learning, DL）：深度學習是機器學習的一個分支，其中人工神經網絡通過組合多層人造神經元來找到原始數據中的模式。它在 Google DeepMind 的 AlphaGo AI 中得到了廣泛應用。 自然語言處理（Natural Language Processing, NLU）：NLU 是人工智能研究中的一個非常重要的優先事項和挑戰。它使 AI 能夠理解和回應人類的自然語言。 語義感知（Context Awareness）：AI 需要理解情境與上下文的關係，以更好地回應人類需求。",
     "description": "人工智慧（Artificial Intelligence, AI）可以定義為模仿人類與人類思維相關的認知功能的機器或計算機，如學習和解決問題。它是計算機科學的一個分支，能夠感知其環境並採取行動，最大限度地提高其成功機會。此外，人工智慧能夠從過去的經驗中學習，做出合理的決策，並快速回應。因此，人工智慧研究人員的科學目標是通過構建具有象徵意義的推理或推理的計算機程式來理解智慧。\n以下是一些與人工智慧相關的關鍵詞：\n機器學習（Machine Learning, ML）：機器學習是 AI 交響樂中的一個工具，它是從先前的數據學習進行運算以預測行為的能力。機器學習最主流的方法是向算法顯示情境的數據集，並透過訓練模型告訴它什麼是正確的決定。 深度學習（Deep Learning, DL）：深度學習是機器學習的一個分支，其中人工神經網絡通過組合多層人造神經元來找到原始數據中的模式。它在 Google DeepMind 的 AlphaGo AI 中得到了廣泛應用。 自然語言處理（Natural Language Processing, NLU）：NLU 是人工智能研究中的一個非常重要的優先事項和挑戰。它使 AI 能夠理解和回應人類的自然語言。 語義感知（Context Awareness）：AI 需要理解情境與上下文的關係，以更好地回應人類需求。",
     "tags": [],
@@ -16,63 +8,7 @@ var relearn_searchindex = [
     "uri": "/sw/ai/index.html"
   },
   {
-    "breadcrumb": "首頁 \u003e \r軟體工程",
-    "content": "電腦電話整合（Computer Telephony Integration, CTI）技術是一項將電腦和電話結合在一起的新技術。它提供了一個強大的整合功能，讓使用者可以在單一介面上操作並享受整合性的功能。以下是電腦電話整合的一些功能：\n來電者資訊視窗彈跳（Screen-Pop）：當有來電時，系統會自動彈出一個視窗，顯示來電者的相關資訊，例如姓名、公司、職位等。這有助於提高客戶服務效率，讓接聽者能更快地識別來電者。 與後端客戶資料庫整合：電腦電話整合可以直接透過電腦操作電話功能，例如查詢客戶資料、新增筆記、更新客戶狀態等。這有助於提高工作效率，不需要在電話和電腦之間來回切換。 傳真功能整合：一些整合工具可以將傳真功能直接整合至通訊平台，例如 Zoom Phone。這讓您可以透過同一個介面傳送和接收傳真，而不需要切換不同的應用程式。 合規機制整合：如果您在高度監管的行業工作，例如金融服務業，電腦電話整合工具可以將必要的封存和資料保護功能整合至語音通話、語音信箱和短信訊息中，以確保合規性並保障完整體驗。",
-    "description": "電腦電話整合（Computer Telephony Integration, CTI）技術是一項將電腦和電話結合在一起的新技術。它提供了一個強大的整合功能，讓使用者可以在單一介面上操作並享受整合性的功能。以下是電腦電話整合的一些功能：\n來電者資訊視窗彈跳（Screen-Pop）：當有來電時，系統會自動彈出一個視窗，顯示來電者的相關資訊，例如姓名、公司、職位等。這有助於提高客戶服務效率，讓接聽者能更快地識別來電者。 與後端客戶資料庫整合：電腦電話整合可以直接透過電腦操作電話功能，例如查詢客戶資料、新增筆記、更新客戶狀態等。這有助於提高工作效率，不需要在電話和電腦之間來回切換。 傳真功能整合：一些整合工具可以將傳真功能直接整合至通訊平台，例如 Zoom Phone。這讓您可以透過同一個介面傳送和接收傳真，而不需要切換不同的應用程式。 合規機制整合：如果您在高度監管的行業工作，例如金融服務業，電腦電話整合工具可以將必要的封存和資料保護功能整合至語音通話、語音信箱和短信訊息中，以確保合規性並保障完整體驗。",
-    "tags": [],
-    "title": "Computer Telephony Integration (電腦電話整合)",
-    "uri": "/sw/cti/index.html"
-  },
-  {
-    "breadcrumb": "首頁 \u003e \r資安工程",
-    "content": "網路威脅情資 威脅情資 (Cyber Threat Intelligence, CTI)，也稱為網路威脅情資或資安情資，是指與網路攻擊相關的資訊，經由專業團隊收集、轉換、分析、解釋等處理過程，提供資安決策所需的基礎。在現今的網路戰爭中，威脅情資對企業的資安防禦布局至關重要。透過威脅情資，企業能夠有效規劃資安防禦計畫，包括資安意識教育、資安解決方案的導入、資訊系統設定的調整，並區分輕重緩急，分配預算。\n威脅情資可分為三個層次，每個層次都對企業的資安計畫帶來效益：\n戰略型威脅情資（Strategic Threat Intelligence）：無技術細節，由高階主管使用，用於辨別攻擊者的動機和目的。 實戰型威脅情資（Operational Threat Intelligence）：需要技術背景，由安全運營中心（SOC）主管和分析研究員使用，用於理解攻擊者的戰術、技術和流程。 戰術型威脅情資（Tactical Threat Intelligence）：需要技術背景，通常由SOC人員使用，以入侵指標（IoC）監控特定攻擊事件。",
-    "description": "網路威脅情資 威脅情資 (Cyber Threat Intelligence, CTI)，也稱為網路威脅情資或資安情資，是指與網路攻擊相關的資訊，經由專業團隊收集、轉換、分析、解釋等處理過程，提供資安決策所需的基礎。在現今的網路戰爭中，威脅情資對企業的資安防禦布局至關重要。透過威脅情資，企業能夠有效規劃資安防禦計畫，包括資安意識教育、資安解決方案的導入、資訊系統設定的調整，並區分輕重緩急，分配預算。\n威脅情資可分為三個層次，每個層次都對企業的資安計畫帶來效益：\n戰略型威脅情資（Strategic Threat Intelligence）：無技術細節，由高階主管使用，用於辨別攻擊者的動機和目的。 實戰型威脅情資（Operational Threat Intelligence）：需要技術背景，由安全運營中心（SOC）主管和分析研究員使用，用於理解攻擊者的戰術、技術和流程。 戰術型威脅情資（Tactical Threat Intelligence）：需要技術背景，通常由SOC人員使用，以入侵指標（IoC）監控特定攻擊事件。",
-    "tags": [],
-    "title": "Cyber Threat Intelligence (網路威脅情資)",
-    "uri": "/cs/cti/index.html"
-  },
-  {
-    "breadcrumb": "首頁 \u003e \r資安工程",
-    "content": "ISO 與 NIST",
-    "description": "ISO 與 NIST",
-    "tags": [],
-    "title": "ISO＆NIST",
-    "uri": "/cs/iso_nist/index.html"
-  },
-  {
-    "breadcrumb": "首頁 \u003e \r管理工程",
-    "content": "專案管理（Project Management，PM） 是一門與產品的規劃、研發、設計、行銷和其他有關產品生產和分銷的學問。它更深入地考量產品研發的時程、成本和人力等各種資源運用調度。專案管理涉及各個部門、團隊、資訊、企業SOP和IT系統的運籌帷幄。\n專案管理的目的是產生一個符合客戶目標的完整專案。在許多情景下，它也可以通過塑造或改變客戶的想法，更切實地實現客戶的目標。一旦明確了客戶的目標，專案管理確保目標指導參與專案的其他人員，例如專案經理、設計師、承包商和分包商等，做出的所有決策。\n專案管理的重要性體現在以下幾個方面：\n協調和統籌：由於專案可能涉及多個團隊和不同的技術領域，專業的專案管理確保各方順利協作，減少溝通障礙，提高工作效率。\n可追蹤性和透明度：專案管理有助於確保專案的可追蹤性，讓團隊能夠清晰地了解專案的進度和狀態。",
-    "description": "專案管理（Project Management，PM） 是一門與產品的規劃、研發、設計、行銷和其他有關產品生產和分銷的學問。它更深入地考量產品研發的時程、成本和人力等各種資源運用調度。專案管理涉及各個部門、團隊、資訊、企業SOP和IT系統的運籌帷幄。\n專案管理的目的是產生一個符合客戶目標的完整專案。在許多情景下，它也可以通過塑造或改變客戶的想法，更切實地實現客戶的目標。一旦明確了客戶的目標，專案管理確保目標指導參與專案的其他人員，例如專案經理、設計師、承包商和分包商等，做出的所有決策。\n專案管理的重要性體現在以下幾個方面：\n協調和統籌：由於專案可能涉及多個團隊和不同的技術領域，專業的專案管理確保各方順利協作，減少溝通障礙，提高工作效率。\n可追蹤性和透明度：專案管理有助於確保專案的可追蹤性，讓團隊能夠清晰地了解專案的進度和狀態。",
-    "tags": [],
-    "title": "專案管理",
-    "uri": "/ms/pmp/index.html"
-  },
-  {
-    "breadcrumb": "首頁",
-    "content": "管理分享",
-    "description": "管理分享",
-    "tags": [],
-    "title": "管理工程",
-    "uri": "/ms/index.html"
-  },
-  {
-    "breadcrumb": "首頁 \u003e \r管理工程",
-    "content": "能力成熟度模型整合（CMMI）是一種改進過程的方法，旨在協助提升組織的績效。它可用於引導整個專案、部門甚至整個組織的過程改進。在軟體工程和組織發展領域中，CMMI提供了有效過程改進的基本元素。它由卡內基梅隆大學在美國專利和商標局註冊。\nCMMI的主要目的包括：\n協助整合傳統獨立的組織功能。\n設定過程改進目標和優先級。\n提供品質過程的指引。\n為評價當前過程提供參考點.\nCMMI涵蓋了三個感興趣的區域：\n產品和服務開發（CMMI-DEV）：用於開發過程改進。\n服務建立、管理和交付（CMMI-SVC）：用於服務過程改進。\n產品和服務採購（CMMI-ACQ）：用於採購過程改進。\nCMMI不再僅限於軟體工程，而是被廣泛應用於硬體產品開發、各種業務的交付以及產品和服務的採購。它提供了一個統一的框架，以改善組織的過程成熟度和績效。",
-    "description": "能力成熟度模型整合（CMMI）是一種改進過程的方法，旨在協助提升組織的績效。它可用於引導整個專案、部門甚至整個組織的過程改進。在軟體工程和組織發展領域中，CMMI提供了有效過程改進的基本元素。它由卡內基梅隆大學在美國專利和商標局註冊。\nCMMI的主要目的包括：\n協助整合傳統獨立的組織功能。\n設定過程改進目標和優先級。\n提供品質過程的指引。\n為評價當前過程提供參考點.\nCMMI涵蓋了三個感興趣的區域：\n產品和服務開發（CMMI-DEV）：用於開發過程改進。\n服務建立、管理和交付（CMMI-SVC）：用於服務過程改進。\n產品和服務採購（CMMI-ACQ）：用於採購過程改進。\nCMMI不再僅限於軟體工程，而是被廣泛應用於硬體產品開發、各種業務的交付以及產品和服務的採購。它提供了一個統一的框架，以改善組織的過程成熟度和績效。",
-    "tags": [],
-    "title": "能力成熟度模型整合",
-    "uri": "/ms/cmmi/index.html"
-  },
-  {
-    "breadcrumb": "首頁",
-    "content": "資安工程分享.",
-    "description": "資安工程分享.",
-    "tags": [],
-    "title": "資安工程",
-    "uri": "/cs/index.html"
-  },
-  {
-    "breadcrumb": "首頁",
+    "breadcrumb": "",
     "content": "軟體工程.",
     "description": "軟體工程.",
     "tags": [],
@@ -80,20 +16,76 @@ var relearn_searchindex = [
     "uri": "/sw/index.html"
   },
   {
-    "breadcrumb": "首頁",
-    "content": "工具介紹",
-    "description": "工具介紹",
+    "breadcrumb": "軟體工程",
+    "content": "電腦電話整合（Computer Telephony Integration, CTI）技術是一項將電腦和電話結合在一起的新技術。它提供了一個強大的整合功能，讓使用者可以在單一介面上操作並享受整合性的功能。以下是電腦電話整合的一些功能：\n來電者資訊視窗彈跳（Screen-Pop）：當有來電時，系統會自動彈出一個視窗，顯示來電者的相關資訊，例如姓名、公司、職位等。這有助於提高客戶服務效率，讓接聽者能更快地識別來電者。 與後端客戶資料庫整合：電腦電話整合可以直接透過電腦操作電話功能，例如查詢客戶資料、新增筆記、更新客戶狀態等。這有助於提高工作效率，不需要在電話和電腦之間來回切換。 傳真功能整合：一些整合工具可以將傳真功能直接整合至通訊平台，例如 Zoom Phone。這讓您可以透過同一個介面傳送和接收傳真，而不需要切換不同的應用程式。 合規機制整合：如果您在高度監管的行業工作，例如金融服務業，電腦電話整合工具可以將必要的封存和資料保護功能整合至語音通話、語音信箱和短信訊息中，以確保合規性並保障完整體驗。",
+    "description": "電腦電話整合（Computer Telephony Integration, CTI）技術是一項將電腦和電話結合在一起的新技術。它提供了一個強大的整合功能，讓使用者可以在單一介面上操作並享受整合性的功能。以下是電腦電話整合的一些功能：\n來電者資訊視窗彈跳（Screen-Pop）：當有來電時，系統會自動彈出一個視窗，顯示來電者的相關資訊，例如姓名、公司、職位等。這有助於提高客戶服務效率，讓接聽者能更快地識別來電者。 與後端客戶資料庫整合：電腦電話整合可以直接透過電腦操作電話功能，例如查詢客戶資料、新增筆記、更新客戶狀態等。這有助於提高工作效率，不需要在電話和電腦之間來回切換。 傳真功能整合：一些整合工具可以將傳真功能直接整合至通訊平台，例如 Zoom Phone。這讓您可以透過同一個介面傳送和接收傳真，而不需要切換不同的應用程式。 合規機制整合：如果您在高度監管的行業工作，例如金融服務業，電腦電話整合工具可以將必要的封存和資料保護功能整合至語音通話、語音信箱和短信訊息中，以確保合規性並保障完整體驗。",
     "tags": [],
-    "title": "一般工具",
-    "uri": "/tools/index.html"
+    "title": "Computer Telephony Integration (電腦電話整合)",
+    "uri": "/sw/cti/index.html"
   },
   {
-    "breadcrumb": "首頁 \u003e \r一般工具",
-    "content": "在學術寫作和論文編排領域，LaTeX 是許多研究者和學生的首選工具。而 TeXstudio 作為功能強大的 LaTeX 編輯器，提供了直觀的介面與豐富的功能，使論文寫作更加高效。本篇文章將介紹 TeXstudio 的安裝及使用 ，幫助你輕鬆上手，提升寫作效率。\n1. 為何選擇 TeXstudio？ 使用 TeXstudio 的主要優勢包括：\n自動補全 ：支援 LaTeX 代碼補全，提高輸入速度\n即時預覽 ：可查看 PDF 生成結果，避免格式錯誤\n整合檢查工具 ：內建拼寫檢查、語法高亮與錯誤提示\n跨平台支持 ：可在 Windows、macOS 和 Linux 上運行\n2. TeXstudio 安裝步驟 （1）下載並安裝 TeXstudio 前往 TeXstudio 官方網站 下載適用的版本：\n進入下載頁面，選擇適合的作業系統（Windows、macOS 或 Linux）。 下載後，按照安裝提示進行安裝。 （2）安裝 LaTeX 發行版 TeXstudio 需要 LaTeX 發行版來編譯文件，你可以選擇以下其中之一：\nWindows ：建議安裝MiKTeX MacOS ：推薦MacTeX Linux ：可使用 TeX Live（通常可透過系統軟件管理器安裝） 安裝完成後，請確保 TeXstudio 能夠找到 LaTeX 發行版，以正確編譯文件。\n3. TeXstudio 基本使用 （1）新建與編輯 LaTeX 文檔 打開 TeXstudio，選擇File → New 創建新文件。 在編輯區域輸入基本 LaTeX 代碼，例如： \\documentclass[conference]{IEEEtran} \\usepackage{fontspec} %可設定定字體 \\usepackage{xeCJK} %中英文字體分開 \\setCJKmainfont{標楷體} %設定中文指定字型，英文使用原字型 \\XeTeXlinebreaklocale \"zh\" %以中文方式自動換行 \\XeTeXlinebreakskip = 0pt plus 1pt %要加這兩行，中文才能自動換行 \\begin{document} \\section{大綱} 大綱，中英文 \\\\ Hello World! \\section{介紹}\t文章內容 2025年 \\end{document}\r按Ctrl + S 保存文件，建議使用.tex 作為副檔名。 （2）編譯與預覽 點擊Build \u0026 View（F5） 按鈕進行編譯。\n如果安裝成功，右側將顯示生成的 PDF 文檔。\n（3）常用快捷鍵 功能 快捷鍵 編譯文件 F5 自動補全 Ctrl + Space 注釋/取消注釋 Ctrl + T 4. 進階技巧 （1）加入 BibTeX 參考文獻 在學術論文中，引用文獻至關重要，可使用 BibTeX 進行文獻管理：\n創建.bib 文件，並添加文獻條目：(例如存為 document.bib)\n@article{minaee2025largelanguagemodelssurvey, title={Large Language Models: A Survey}, author={Shervin Minaee and Tomas Mikolov and Narjes Nikzad and Meysam Chenaghlu and Richard Socher and Xavier Amatriain and Jianfeng Gao}, year={2025}, eprint={2402.06196}, archivePrefix={arXiv}, primaryClass={cs.CL}, url={https://arxiv.org/abs/2402.06196}, }\r在主.tex 文件中引入：\n\\bibliographystyle{plain} \\bibliography{references}\r可引用參考文獻\n（2）使用論文樣式 直接在各論文的樣版文章下載LaTex樣版，如：Manuscript Templates for Conference Proceedings | IEEE，直接使用格式進行修改及論文撰寫。\n5. 結論 TeXstudio 是功能強大、適合初學者與進階使用者的 LaTeX 編輯器，透過其友善的介面與強大功能，你可以更高效地編寫學術論文。如果你正在尋找一款理想的 LaTeX 工具，不妨試試 TeXstudio，讓你的論文排版更加流暢！",
-    "description": "在學術寫作和論文編排領域，LaTeX 是許多研究者和學生的首選工具。而 TeXstudio 作為功能強大的 LaTeX 編輯器，提供了直觀的介面與豐富的功能，使論文寫作更加高效。本篇文章將介紹 TeXstudio 的安裝及使用 ，幫助你輕鬆上手，提升寫作效率。\n1. 為何選擇 TeXstudio？ 使用 TeXstudio 的主要優勢包括：\n自動補全 ：支援 LaTeX 代碼補全，提高輸入速度\n即時預覽 ：可查看 PDF 生成結果，避免格式錯誤\n整合檢查工具 ：內建拼寫檢查、語法高亮與錯誤提示\n跨平台支持 ：可在 Windows、macOS 和 Linux 上運行\n2. TeXstudio 安裝步驟 （1）下載並安裝 TeXstudio 前往 TeXstudio 官方網站 下載適用的版本：\n進入下載頁面，選擇適合的作業系統（Windows、macOS 或 Linux）。 下載後，按照安裝提示進行安裝。 （2）安裝 LaTeX 發行版 TeXstudio 需要 LaTeX 發行版來編譯文件，你可以選擇以下其中之一：",
+    "breadcrumb": "資安工程",
+    "content": "網路威脅情資 威脅情資 (Cyber Threat Intelligence, CTI)，也稱為網路威脅情資或資安情資，是指與網路攻擊相關的資訊，經由專業團隊收集、轉換、分析、解釋等處理過程，提供資安決策所需的基礎。在現今的網路戰爭中，威脅情資對企業的資安防禦布局至關重要。透過威脅情資，企業能夠有效規劃資安防禦計畫，包括資安意識教育、資安解決方案的導入、資訊系統設定的調整，並區分輕重緩急，分配預算。\n威脅情資可分為三個層次，每個層次都對企業的資安計畫帶來效益：\n戰略型威脅情資（Strategic Threat Intelligence）：無技術細節，由高階主管使用，用於辨別攻擊者的動機和目的。 實戰型威脅情資（Operational Threat Intelligence）：需要技術背景，由安全運營中心（SOC）主管和分析研究員使用，用於理解攻擊者的戰術、技術和流程。 戰術型威脅情資（Tactical Threat Intelligence）：需要技術背景，通常由SOC人員使用，以入侵指標（IoC）監控特定攻擊事件。",
+    "description": "網路威脅情資 威脅情資 (Cyber Threat Intelligence, CTI)，也稱為網路威脅情資或資安情資，是指與網路攻擊相關的資訊，經由專業團隊收集、轉換、分析、解釋等處理過程，提供資安決策所需的基礎。在現今的網路戰爭中，威脅情資對企業的資安防禦布局至關重要。透過威脅情資，企業能夠有效規劃資安防禦計畫，包括資安意識教育、資安解決方案的導入、資訊系統設定的調整，並區分輕重緩急，分配預算。\n威脅情資可分為三個層次，每個層次都對企業的資安計畫帶來效益：\n戰略型威脅情資（Strategic Threat Intelligence）：無技術細節，由高階主管使用，用於辨別攻擊者的動機和目的。 實戰型威脅情資（Operational Threat Intelligence）：需要技術背景，由安全運營中心（SOC）主管和分析研究員使用，用於理解攻擊者的戰術、技術和流程。 戰術型威脅情資（Tactical Threat Intelligence）：需要技術背景，通常由SOC人員使用，以入侵指標（IoC）監控特定攻擊事件。",
     "tags": [],
-    "title": "論文寫作工具：TeXstudio 安裝及使用指南",
-    "uri": "/tools/texstudio/index.html"
+    "title": "Cyber Threat Intelligence (網路威脅情資)",
+    "uri": "/cs/cti/index.html"
+  },
+  {
+    "breadcrumb": "資安工程",
+    "content": "ISO 與 NIST",
+    "description": "ISO 與 NIST",
+    "tags": [],
+    "title": "ISO＆NIST",
+    "uri": "/cs/iso_nist/index.html"
+  },
+  {
+    "breadcrumb": "軟體工程",
+    "content": "Vibe Coding是一種全新的程式設計方式，開發者透過自然語言向AI描述需求，由AI自動生成程式碼，其核心理念是 「完全順應感覺（vibes）、擁抱AI帶來的進步，並忘記程式碼本身的存在」",
+    "description": "Vibe Coding是一種全新的程式設計方式，開發者透過自然語言向AI描述需求，由AI自動生成程式碼，其核心理念是 「完全順應感覺（vibes）、擁抱AI帶來的進步，並忘記程式碼本身的存在」",
+    "tags": [],
+    "title": "Vibe Coding (氛圍編程)",
+    "uri": "/sw/vibecoding/index.html"
+  },
+  {
+    "breadcrumb": "管理工程",
+    "content": "專案管理（Project Management，PM） 是一門與產品的規劃、研發、設計、行銷和其他有關產品生產和分銷的學問。它更深入地考量產品研發的時程、成本和人力等各種資源運用調度。專案管理涉及各個部門、團隊、資訊、企業SOP和IT系統的運籌帷幄。\n專案管理的目的是產生一個符合客戶目標的完整專案。在許多情景下，它也可以通過塑造或改變客戶的想法，更切實地實現客戶的目標。一旦明確了客戶的目標，專案管理確保目標指導參與專案的其他人員，例如專案經理、設計師、承包商和分包商等，做出的所有決策。\n專案管理的重要性體現在以下幾個方面：\n協調和統籌：由於專案可能涉及多個團隊和不同的技術領域，專業的專案管理確保各方順利協作，減少溝通障礙，提高工作效率。\n可追蹤性和透明度：專案管理有助於確保專案的可追蹤性，讓團隊能夠清晰地了解專案的進度和狀態。",
+    "description": "專案管理（Project Management，PM） 是一門與產品的規劃、研發、設計、行銷和其他有關產品生產和分銷的學問。它更深入地考量產品研發的時程、成本和人力等各種資源運用調度。專案管理涉及各個部門、團隊、資訊、企業SOP和IT系統的運籌帷幄。\n專案管理的目的是產生一個符合客戶目標的完整專案。在許多情景下，它也可以通過塑造或改變客戶的想法，更切實地實現客戶的目標。一旦明確了客戶的目標，專案管理確保目標指導參與專案的其他人員，例如專案經理、設計師、承包商和分包商等，做出的所有決策。\n專案管理的重要性體現在以下幾個方面：\n協調和統籌：由於專案可能涉及多個團隊和不同的技術領域，專業的專案管理確保各方順利協作，減少溝通障礙，提高工作效率。\n可追蹤性和透明度：專案管理有助於確保專案的可追蹤性，讓團隊能夠清晰地了解專案的進度和狀態。",
+    "tags": [],
+    "title": "專案管理",
+    "uri": "/ms/pmp/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "管理分享",
+    "description": "管理分享",
+    "tags": [],
+    "title": "管理工程",
+    "uri": "/ms/index.html"
+  },
+  {
+    "breadcrumb": "管理工程",
+    "content": "能力成熟度模型整合（CMMI）是一種改進過程的方法，旨在協助提升組織的績效。它可用於引導整個專案、部門甚至整個組織的過程改進。在軟體工程和組織發展領域中，CMMI提供了有效過程改進的基本元素。它由卡內基梅隆大學在美國專利和商標局註冊。\nCMMI的主要目的包括：\n協助整合傳統獨立的組織功能。\n設定過程改進目標和優先級。\n提供品質過程的指引。\n為評價當前過程提供參考點.\nCMMI涵蓋了三個感興趣的區域：\n產品和服務開發（CMMI-DEV）：用於開發過程改進。\n服務建立、管理和交付（CMMI-SVC）：用於服務過程改進。\n產品和服務採購（CMMI-ACQ）：用於採購過程改進。\nCMMI不再僅限於軟體工程，而是被廣泛應用於硬體產品開發、各種業務的交付以及產品和服務的採購。它提供了一個統一的框架，以改善組織的過程成熟度和績效。",
+    "description": "能力成熟度模型整合（CMMI）是一種改進過程的方法，旨在協助提升組織的績效。它可用於引導整個專案、部門甚至整個組織的過程改進。在軟體工程和組織發展領域中，CMMI提供了有效過程改進的基本元素。它由卡內基梅隆大學在美國專利和商標局註冊。\nCMMI的主要目的包括：\n協助整合傳統獨立的組織功能。\n設定過程改進目標和優先級。\n提供品質過程的指引。\n為評價當前過程提供參考點.\nCMMI涵蓋了三個感興趣的區域：\n產品和服務開發（CMMI-DEV）：用於開發過程改進。\n服務建立、管理和交付（CMMI-SVC）：用於服務過程改進。\n產品和服務採購（CMMI-ACQ）：用於採購過程改進。\nCMMI不再僅限於軟體工程，而是被廣泛應用於硬體產品開發、各種業務的交付以及產品和服務的採購。它提供了一個統一的框架，以改善組織的過程成熟度和績效。",
+    "tags": [],
+    "title": "能力成熟度模型整合",
+    "uri": "/ms/cmmi/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "資安工程分享.",
+    "description": "資安工程分享.",
+    "tags": [],
+    "title": "資安工程",
+    "uri": "/cs/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "擁有多元化的專業知識和超過20年的資訊科技工作經驗。致力於領導創新團隊，開發AI客服和CRM解決方案。目前正在攻讀大同大學的資工博士學位，為資安和AI領域結合理論和實務。",
+    "description": "擁有多元化的專業知識和超過20年的資訊科技工作經驗。致力於領導創新團隊，開發AI客服和CRM解決方案。目前正在攻讀大同大學的資工博士學位，為資安和AI領域結合理論和實務。",
+    "tags": [],
+    "title": "關於我",
+    "uri": "/me/index.html"
   },
   {
     "breadcrumb": "",
@@ -104,7 +96,23 @@ var relearn_searchindex = [
     "uri": "/index.html"
   },
   {
-    "breadcrumb": "首頁",
+    "breadcrumb": "",
+    "content": "工具介紹",
+    "description": "工具介紹",
+    "tags": [],
+    "title": "一般工具",
+    "uri": "/tools/index.html"
+  },
+  {
+    "breadcrumb": "一般工具",
+    "content": "在學術寫作和論文編排領域，LaTeX 是許多研究者和學生的首選工具。而 TeXstudio 作為功能強大的 LaTeX 編輯器，提供了直觀的介面與豐富的功能，使論文寫作更加高效。本篇文章將介紹 TeXstudio 的安裝及使用 ，幫助你輕鬆上手，提升寫作效率。\n1. 為何選擇 TeXstudio？ 使用 TeXstudio 的主要優勢包括：\n自動補全 ：支援 LaTeX 代碼補全，提高輸入速度\n即時預覽 ：可查看 PDF 生成結果，避免格式錯誤\n整合檢查工具 ：內建拼寫檢查、語法高亮與錯誤提示\n跨平台支持 ：可在 Windows、macOS 和 Linux 上運行\n2. TeXstudio 安裝步驟 （1）下載並安裝 TeXstudio 前往 TeXstudio 官方網站 下載適用的版本：\n進入下載頁面，選擇適合的作業系統（Windows、macOS 或 Linux）。 下載後，按照安裝提示進行安裝。 （2）安裝 LaTeX 發行版 TeXstudio 需要 LaTeX 發行版來編譯文件，你可以選擇以下其中之一：\nWindows ：建議安裝MiKTeX MacOS ：推薦MacTeX Linux ：可使用 TeX Live（通常可透過系統軟件管理器安裝） 安裝完成後，請確保 TeXstudio 能夠找到 LaTeX 發行版，以正確編譯文件。\n3. TeXstudio 基本使用 （1）新建與編輯 LaTeX 文檔 打開 TeXstudio，選擇File → New 創建新文件。 在編輯區域輸入基本 LaTeX 代碼，例如： \\documentclass[conference]{IEEEtran} \\usepackage{fontspec} %可設定定字體 \\usepackage{xeCJK} %中英文字體分開 \\setCJKmainfont{標楷體} %設定中文指定字型，英文使用原字型 \\XeTeXlinebreaklocale \"zh\" %以中文方式自動換行 \\XeTeXlinebreakskip = 0pt plus 1pt %要加這兩行，中文才能自動換行 \\begin{document} \\section{大綱} 大綱，中英文 \\\\ Hello World! \\section{介紹}\t文章內容 2025年 \\end{document}\r按Ctrl + S 保存文件，建議使用.tex 作為副檔名。 （2）編譯與預覽 點擊Build \u0026 View（F5） 按鈕進行編譯。\n如果安裝成功，右側將顯示生成的 PDF 文檔。\n（3）常用快捷鍵 功能 快捷鍵 編譯文件 F5 自動補全 Ctrl + Space 注釋/取消注釋 Ctrl + T 4. 進階技巧 （1）加入 BibTeX 參考文獻 在學術論文中，引用文獻至關重要，可使用 BibTeX 進行文獻管理：\n創建.bib 文件，並添加文獻條目：(例如存為 document.bib)\n@article{minaee2025largelanguagemodelssurvey, title={Large Language Models: A Survey}, author={Shervin Minaee and Tomas Mikolov and Narjes Nikzad and Meysam Chenaghlu and Richard Socher and Xavier Amatriain and Jianfeng Gao}, year={2025}, eprint={2402.06196}, archivePrefix={arXiv}, primaryClass={cs.CL}, url={https://arxiv.org/abs/2402.06196}, }\r在主.tex 文件中引入：\n\\bibliographystyle{plain} \\bibliography{references}\r可引用參考文獻\n（2）使用論文樣式 直接在各論文的樣版文章下載LaTex樣版，如：Manuscript Templates for Conference Proceedings | IEEE，直接使用格式進行修改及論文撰寫。\n5. 結論 TeXstudio 是功能強大、適合初學者與進階使用者的 LaTeX 編輯器，透過其友善的介面與強大功能，你可以更高效地編寫學術論文。如果你正在尋找一款理想的 LaTeX 工具，不妨試試 TeXstudio，讓你的論文排版更加流暢！",
+    "description": "在學術寫作和論文編排領域，LaTeX 是許多研究者和學生的首選工具。而 TeXstudio 作為功能強大的 LaTeX 編輯器，提供了直觀的介面與豐富的功能，使論文寫作更加高效。本篇文章將介紹 TeXstudio 的安裝及使用 ，幫助你輕鬆上手，提升寫作效率。\n1. 為何選擇 TeXstudio？ 使用 TeXstudio 的主要優勢包括：\n自動補全 ：支援 LaTeX 代碼補全，提高輸入速度\n即時預覽 ：可查看 PDF 生成結果，避免格式錯誤\n整合檢查工具 ：內建拼寫檢查、語法高亮與錯誤提示\n跨平台支持 ：可在 Windows、macOS 和 Linux 上運行\n2. TeXstudio 安裝步驟 （1）下載並安裝 TeXstudio 前往 TeXstudio 官方網站 下載適用的版本：\n進入下載頁面，選擇適合的作業系統（Windows、macOS 或 Linux）。 下載後，按照安裝提示進行安裝。 （2）安裝 LaTeX 發行版 TeXstudio 需要 LaTeX 發行版來編譯文件，你可以選擇以下其中之一：",
+    "tags": [],
+    "title": "論文寫作工具：TeXstudio 安裝及使用指南",
+    "uri": "/tools/texstudio/index.html"
+  },
+  {
+    "breadcrumb": "",
     "content": "",
     "description": "",
     "tags": [],
@@ -112,7 +120,7 @@ var relearn_searchindex = [
     "uri": "/categories/index.html"
   },
   {
-    "breadcrumb": "首頁",
+    "breadcrumb": "",
     "content": "",
     "description": "",
     "tags": [],
